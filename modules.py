c22cff2b6d822ca991a2619f7d39729e25b63a73
KabongeHajarah <kabongehajarah@gmail.com>
2023-05-31 23:43:23 +0300

    modules_built_in

--- modules.py
+++ modules.py
@@ -13,3 +13,70 @@ print(generate_full_name('Asabneh','Yetayeh'))
 #Import Functions from a Module and Renaming
 from modules import generate_full_name as full_name
 print(full_name('Asabneh','Yetayeh'))
+
+
+#Import Built-in Modules
+# import the module
+import os
+# Creating a directory
+os.mkdir('directory_name')
+# Changing the current directory
+os.chdir('path')
+# Getting current working directory
+os.getcwd()
+# Removing directory
+os.rmdir()
+
+
+import sys
+#print(sys.argv[0], argv[1],sys.argv[2])  # this line would print out: filename argument1 argument2
+print('Welcome {}. Enjoy  {} challenge!'.format(sys.argv[1], sys.argv[2]))
+
+
+#Statistics Module
+from statistics import * # importing all the statistics modules
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))       # ~22.9
+print(median(ages))     # 23
+print(mode(ages))       # 20
+print(stdev(ages))      # ~2.3
+
+
+#Math modules
+import math
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(2))      # 1.4142135623730951, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
+  
+from math import pi
+print(pi)
+#It is also possible to import multiple functions at once
+
+from math import pi, sqrt, pow, floor, ceil, log10
+print(pi)                 # 3.141592653589793
+print(sqrt(2))            # 1.4142135623730951
+print(pow(2, 3))          # 8.0
+print(floor(9.81))        # 9
+print(ceil(9.81))         # 10
+print(math.log10(100))    # 2
+
+#To check what functions the module has got, we can use help(math), or dir(math)
+
+
+#We can also rename the name of the function
+from math import pi as  PI
+print(PI) # 3.141592653589793
+
+#String Module
+import string
+print(string.ascii_letters) # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.digits)        # 0123456789
+print(string.punctuation)   # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+#Random Module
+from random import random, randint
+print(random())   # it doesn't take any arguments; it returns a value between 0 and 0.9999
+print(randint(5, 20)) # it returns a random integer number between [5, 20] inclusive
